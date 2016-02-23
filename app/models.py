85dd6f2d3cc000eb6917b77382f03859a1c2cd6d
daortellado <daortellado@gmail.com>
2016-02-22 19:31:09 -0500

    fix data shit

--- app/models.py
+++ app/models.py
@@ -38,4 +38,4 @@ class Tag(Model):
     subject_es = Column(String(120))
 
     def __repr__(self):
-        return "{0},{1}".format(self.subject_en, self.subject_es)
+        return u"{0},{1}".format(self.subject_en, self.subject_es)
